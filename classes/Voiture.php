20f67ec13ba81e98ae24c0ef8f4369a4f374e8ff
Samuel <slanguet@protonmail.com>
2022-07-21 11:56:13 +0200

    ajout private set et get
    ajout private set et get

--- classes/Voiture.php
+++ classes/Voiture.php
@@ -1,11 +1,29 @@
 <?php
 
 class Voiture {
-    public string $marque;
-    public string $couleur;
+    private string $marque;
+    private string $couleur;
     public int $nbrRoues = 4;
+
+    public function getCouleur(): string{
+        return $this->couleur;
+    }
+    public function setCouleur(string $couleur): void{
+        $this->couleur = $couleur;
+    }
+    public function getMarque(): string{
+        return $this->marque;
+    }
+    public function setMarque(string $marque): void{
+        $this->marque = $marque;
+    }
 
     public function demarrer() {
         echo "<p>La voiture démarre</p>";
+    }
+
+    public function afficherCouleur()
+    {
+        echo "<p>La couleur de la voiture est " . $this->couleur . "</p>";
     }
 }
--- index.php
+++ index.php
@@ -3,15 +3,23 @@
 require_once './classes/Voiture.php';
 
 $voiture1 = new Voiture();
-$voiture1->couleur = "rouge";
-$voiture1->marque = "BMW";
-$voiture1->couleur = "vert";
+//$voiture1->couleur = "rouge";
+$voiture1->setCouleur("rouge");
+echo $voiture1->getCouleur();
+//$voiture1->marque = "BMW";
+$voiture1->setMarque("BMW");
+echo $voiture1->getMarque();
+$couleurVoiture1 = $voiture1->afficherCouleur();
+echo $couleurVoiture1;
 
 $voiture2 = new Voiture();
-$voiture2->couleur = "jaune";
-$voiture2->marque = "Mercedes";
+$voiture1->setCouleur("jaune");
+echo $voiture1->getCouleur();
+$voiture2->setMarque("BMW");
+echo $voiture2->getMarque();
+
 $voiture2->demarrer();
 
-var_dump($voiture1);
-echo "<br />";
-var_dump($voiture2);
+// var_dump($voiture1);
+// echo "<br />";
+// var_dump($voiture2);
