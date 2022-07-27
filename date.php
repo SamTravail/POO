2b9eb39280f85101391b93cedd8d70f8502ca2b5
Samuel <slanguet@protonmail.com>
2022-07-27 11:40:40 +0200

    Update date.php
    add test date

--- date.php
+++ date.php
@@ -2,4 +2,22 @@
 
 $paris = new DateTimeZone('Europe/Paris');
 var_dump($paris);
+?>
+<br>
+<?php
+$australie = new DateTimeZone('Australia/Melbourne');
+var_dump($australie->getName(), $australie->getLocation());
 
+$maintenant = new DateTime();
+var_dump($maintenant);
+?>
+    <br>
+<?php
+$samuel = new DateTime('1978-02-18 16:08:00', $paris);
+
+var_dump($samuel);
+?>
+    <br>
+<?php
+$vendrediProchain = new DateTime('next friday');
+var_dump($vendrediProchain);
